b1de177bcea87c7dc6d3aed38d59fdb6a7f28b12
Md tofaiel <mdtofaielhussaintota@gmail.com>
2025-06-13 21:00:24 +0600

    find maximum GCD

---
+++ 1370A_Maximum_GCD.c
@@ -0,0 +1,16 @@
+//Tofaiel_1
+// here problem is solve to find out maximum GCD
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while(t--) {
+        int n;
+        cin >> n;
+        cout << n / 2 << endl;
+    }
+    return 0;
+}
